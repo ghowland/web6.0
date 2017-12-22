a1afbbff74713b914ba8d3bdc1ba3fe848a36368
Geoff Howland <geoff@gmail.com>
2017-12-21 23:56:48 -0800

    Almost working JS Control, vars and strings work

--- web/limitless5/assets/js/web6_js_control.js
+++ web/limitless5/assets/js/web6_js_control.js
@@ -10,16 +10,29 @@ License: MIT
 var __web6_js_control_data_store = new Object()
 var __web6_js_control_data_store_global = new Object()
 
-function JsControl_Register(namespace, dom_id, control_data_list) {
-    __web6_js_control_data_store[namespace] = new Object()
-    __web6_js_control_data_store[namespace]['control_data_list'] = control_data_list
-    __web6_js_control_data_store[namespace]['namespace'] = namespace
-    __web6_js_control_data_store[namespace]['dom_id'] = dom_id
 
-    __web6_js_control_data_store[namespace]['string'] = new Object()
-    __web6_js_control_data_store[namespace]['string_format'] = new Object()
-    __web6_js_control_data_store[namespace]['data'] = new Object()
-    __web6_js_control_data_store[namespace]['var'] = new Object()
+function JsControl_Register(namespace, dom_id, control_data_list) {
+    if (__web6_js_control_data_store[namespace] == undefined) {
+        __web6_js_control_data_store[namespace] = new Object()
+        __web6_js_control_data_store[namespace]['namespace'] = namespace
+
+        __web6_js_control_data_store[namespace]['control_data_list'] = new Array()
+        __web6_js_control_data_store[namespace]['string'] = new Object()
+        __web6_js_control_data_store[namespace]['data'] = new Object()
+        __web6_js_control_data_store[namespace]['var'] = new Object()
+    }
+
+    // Add all the control data items to the list
+    for (item_count in control_data_list) {
+        item = control_data_list[item_count]
+
+        // Add the dom_id here, because we want this to be item specific, but we dont want to have to put it into the item spec, because we dont know the UUID in the spec as that is live data
+        if (item['value_dom'] == undefined) {
+            item['value_dom'] = dom_id
+        }
+
+        __web6_js_control_data_store[namespace]['control_data_list'].push(item)
+    }
 
     JsControl_InitEventHandlers(namespace)
 }
@@ -33,45 +46,27 @@ function JsControl_InitEventHandlers(namespace) {
 
         // Var items -- On change they update the vars data store for this namespace, and then update the Strings and Data
         if (item['type'] === 'var') {
-            if (item['value_dom'] == undefined) {
-                data_store['var'][item['name']] = $("#"+__web6_js_control_data_store[namespace]['dom_id']).val()
+            data_store['var'][item['name']] = $('#' + item['value_dom']).val()
+
+            $('#'+ item['dom_id']).ready().on('change', function (event) {
+                __web6_js_control_data_store[namespace]['var'][item['name']] = $('#' + item['value_dom']).val()
 
                 if (item['publish'] != undefined) {
-                    __web6_js_control_data_store_global[item['publish']] = data_store['var'][item['name']]
+                    __web6_js_control_data_store_global[item['publish']] = __web6_js_control_data_store[namespace]['var'][item['name']]
                 }
 
-                $('#'+ data_store['dom_id']).ready().on('change', function (event) {
-                    __web6_js_control_data_store[namespace]['var'][item['name']] = $("#"+__web6_js_control_data_store[namespace]['dom_id']).val()
-
-                    if (item['publish'] != undefined) {
-                        __web6_js_control_data_store_global[item['publish']] = __web6_js_control_data_store[namespace]['var'][item['name']]
-                    }
-
-                    JsControl_UpdateStringsAndData(namespace)
-                })
-            } else {
-                data_store['var'][item['name']] = $('#' + item['value_dom']).val()
-
-                $('#'+ data_store['dom_id']).ready().on('change', function (event) {
-                    __web6_js_control_data_store[namespace]['var'][item['name']] = $('#' + item['value_dom']).val()
-
-                    if (item['publish'] != undefined) {
-                        __web6_js_control_data_store_global[item['publish']] = __web6_js_control_data_store[namespace]['var'][item['name']]
-                    }
-
-                    JsControl_UpdateStringsAndData(namespace)
-                })
-            }
+                JsControl_UpdateStringsAndData(namespace)
+            })
         }
         // RPC items -- For their specified event type, they execute the RPC
         else if (item['type'] === 'rpc') {
-            $('#'+ data_store['dom_id']).ready().on(item['event'], function (event) {
+            $('#'+ item['dom_id']).ready().on(item['event'], function (event) {
                 RPC(JsControl_Get(namespace, item['name']), item['data'])
             })
         }
         // Eval items -- For their specified event type, they execute the arbitrary code
         else if (item['type'] === 'eval') {
-            $('#'+ data_store['dom_id']).ready().on(item['event'], function (event) {
+            $('#'+ item['dom_id']).ready().on(item['event'], function (event) {
                 eval(item['eval'])
             })
         }
@@ -84,6 +79,8 @@ function JsControl_InitEventHandlers(namespace) {
 function JsControl_UpdateStringsAndData(namespace) {
     data_store = __web6_js_control_data_store[namespace]
 
+    alert('Data Store: ' + namespace + '  Data: ' + JSON.stringify(data_store.toSource()))
+
     //TODO(g): Do data later.  I dont have a use-case for it yet, but it will definitely be implemented as it's useful
     //...
 
@@ -92,10 +89,11 @@ function JsControl_UpdateStringsAndData(namespace) {
         item = data_store['control_data_list'][item_count]
 
         if (item['type'] === 'string') {
+            // Initial format_string, we will iterate over all vars/globals, and replace any template keys in this string
             format_string = item['format']
 
             // Vars
-            for (var_item_access_key in data_store['var']) {
+            for (var var_item_access_key in data_store['var']) {
                 var_item_value = data_store['var'][var_item_access_key]
                 var_item_key = '[[[' + var_item_access_key + ']]]'
 
@@ -104,7 +102,7 @@ function JsControl_UpdateStringsAndData(namespace) {
             }
 
             // Globals
-            for (var_item_access_key in __web6_js_control_data_store_global) {
+            for (var var_item_access_key in __web6_js_control_data_store_global) {
                 var_item_value = __web6_js_control_data_store_global[var_item_access_key]
                 var_item_key = '[[[' + var_item_access_key + ']]]'
 
@@ -112,11 +110,12 @@ function JsControl_UpdateStringsAndData(namespace) {
                 format_string = format_string.replace(var_item_key, var_item_value)
             }
 
-            data_store['string_format'][item['name']] = format_string
+            // After all the formatting, assign it into our formatted dict
+            data_store['string'][item['name']] = format_string
         }
     }
 
-    alert(__web6_js_control_data_store[namespace]['string_format'].toSource())
+    alert('Strings Formatted: ' + JSON.stringify(__web6_js_control_data_store[namespace]['string'].toSource()))
 }
 
 function JsControl_Get(namespace, key) {
