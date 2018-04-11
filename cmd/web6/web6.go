9a593399c6ce6d82774f746988d28cadcd3aabc8
Geoff Howland <geoff@gmail.com>
2018-04-11 11:27:30 -0700

    Adding config flag for web6.conf, so they can be tested quickly by using an argument to specify

--- cmd/web6/web6.go
+++ cmd/web6/web6.go
@@ -11,14 +11,17 @@ import (
 func main() {
 	web6.Start()
 
+	//TODO(g): Process all the gosrv flags ourselves (in web6.Start()) and set the values before starting up, then remove below commented code
+	s := gosrv.New()
+	/*
 	s, err := gosrv.NewFromFlag()
 	if err != nil {
 		panic("Cannot create web server: " + err.Error() + "\n")
-	}
+	}*/
 
 	s.HandleFunc("/", web6.Handler)
 
-	err = s.ListenAndServe()
+	err := s.ListenAndServe()
 	if err != nil {
 		panic("Cannot listen as web server: " + err.Error() + "\n")
 	}
