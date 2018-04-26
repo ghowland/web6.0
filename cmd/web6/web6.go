56f24168c0d00c446978a50ab7c69001baafd132
Geoff Howland <geoff@gmail.com>
2018-04-26 15:18:07 -0700

    Fixing the custom flag options, because gosrv doesnt allow custom flags, so we manage it ourselves

--- cmd/web6/web6.go
+++ cmd/web6/web6.go
@@ -12,16 +12,18 @@ func main() {
 	web6.Start()
 
 	//TODO(g): Process all the gosrv flags ourselves if necessary (in web6.Start()) and set the values before starting up (make sure pid is properly assigned)
-	//s := gosrv.New()
+	s := gosrv.New()
 
+	// We dont use NewFromFlag() because it doesnt allow adding custom flags.  We have to manage the gosrv config Struct ourselves in config.go
+	/*
 	s, err := gosrv.NewFromFlag()
 	if err != nil {
 		panic("Cannot create web server: " + err.Error() + "\n")
-	}
+	}*/
 
 	s.HandleFunc("/", web6.Handler)
 
-	err = s.ListenAndServe()
+	err := s.ListenAndServe()
 	if err != nil {
 		panic("Cannot listen as web server: " + err.Error() + "\n")
 	}
